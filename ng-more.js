162a9a91fb9d633bf2d37f0f2fa6e93f473c4a2a
Gianluca Troiani <gianluca.troiani@gmail.com>
2014-10-17 12:15:43 +0200

    - added watching of element's html - added `ng-more-` prefix to all classes

--- ng-more.js
+++ ng-more.js
@@ -8,7 +8,7 @@ angular.module('ngMore',[])
 		},
 		link: function(scope, elm, attrs) {
 			elm.addClass('ng-more');
-			var more = angular.element('<div>').addClass('more');
+			var more = angular.element('<div>').addClass('ng-more-more');
 			elm.append(more);
 			scope.$watch('sm',function(sm) {
 				showMore(scope.sm,scope.mh);
@@ -16,27 +16,32 @@ angular.module('ngMore',[])
 			scope.$watch('mh',function(mh) {
 				showMore(scope.sm,scope.mh);
 			});
+			scope.$watch(function(){
+				return elm.html();
+			},function() {
+				showMore(scope.sm,scope.mh);
+			});
 			angular.element(window).bind('resize',function(){
 				if (elm[0].offsetHeight < scope.mh) {
-					more.addClass('hide');
+					more.addClass('ng-more-hide');
 				} else {
-					more.removeClass('hide');
+					more.removeClass('ng-more-hide');
 				}
 			});
 			function showMore(sm,mh) {
 				scope.sm = sm = sm === true || sm === "true" || sm === "1";
 				elm.css({'max-height': sm ? '99999em' : mh + 'px'});
 				if(!sm) {
-					elm.removeClass('expanded').addClass('collapsed');
+					elm.removeClass('ng-more-expanded').addClass('ng-more-collapsed');
 					more.html('<i class="fa fa-chevron-down fa-lg"></i>');
 				} else {
-					elm.removeClass('collapsed').addClass('expanded');
+					elm.removeClass('ng-more-collapsed').addClass('ng-more-expanded');
 					more.html('<i class="fa fa-chevron-up fa-lg"></i>');
 				}
 				if (elm[0].offsetHeight < scope.mh) {
-					more.addClass('hide');
+					more.addClass('ng-more-hide');
 				} else {
-					more.removeClass('hide');
+					more.removeClass('ng-more-hide');
 				}
 			}
 			more.bind('click',function(){
